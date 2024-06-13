7d8c7d1a19c36748fa7400e1b2790d0efdf1685a
oguzsancaktar <osncktr.psd@outlook.com.tr>
2024-06-13 18:06:03 +0300

    xa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/',
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
